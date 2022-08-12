68895faac3f161f58cb07038681e995567abfee5
dashakurilovich <Dasha23_@mail.ru>
2022-08-12 16:26:57 +0300

    base scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/alt-text */
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='wrapper'>
+      <header>
+        <div className='headerLeft'>
+          <img width={40} height={40} src='/img/logo.png' />
+          <div className='headerInfo'>
+            <h3>React sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className='headerRight'>
+          <li>
+            <img width={18} height={17} src='/img/cart.svg' />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={20} height={20} src='/img/user.svg' />
+          </li>
+        </ul>
       </header>
+      <div className='content'>
+        <h1>Все кроссовки</h1>
+
+        <div className='sneakers'>
+        <div className='card'>
+          <img width={133} height={112} src='/img/sneakers/1.jpg' alt='Sneakers' />
+          <h5>Мужские Кроссовки Nike Air Max 270</h5>
+          <div className='cardWrapper'>
+            <div className='cardDescribe'>
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className='button'>
+              <img width={11} height={11} src='/img/plus.svg' alt='Plus' />
+            </button>
+          </div>
+        </div>
+        <div className='card'>
+          <img width={133} height={112} src='/img/sneakers/2.jpg' alt='Sneakers' />
+          <h5>Мужские Кроссовки Nike Air Max 270</h5>
+          <div className='cardWrapper'>
+            <div className='cardDescribe'>
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className='button'>
+              <img width={11} height={11} src='/img/plus.svg' alt='Plus' />
+            </button>
+          </div>
+        </div>
+        <div className='card'>
+          <img width={133} height={112} src='/img/sneakers/3.jpg' alt='Sneakers' />
+          <h5>Мужские Кроссовки Nike Air Max 270</h5>
+          <div className='cardWrapper'>
+            <div className='cardDescribe'>
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className='button'>
+              <img width={11} height={11} src='/img/plus.svg' alt='Plus' />
+            </button>
+          </div>
+        </div>
+        <div className='card'>
+          <img width={133} height={112} src='/img/sneakers/4.jpg' alt='Sneakers' />
+          <h5>Мужские Кроссовки Nike Air Max 270</h5>
+          <div className='cardWrapper'>
+            <div className='cardDescribe'>
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className='button'>
+              <img width={11} height={11} src='/img/plus.svg' alt='Plus' />
+            </button>
+          </div>
+        </div>
+        </div>
+
+      </div>
     </div>
   );
 }
