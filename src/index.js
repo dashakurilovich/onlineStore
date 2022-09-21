1cbb0616970acc77af57b1f3cb3d16649a4213fb
dashakurilovich <Dasha23_@mail.ru>
2022-09-21 23:30:58 +0300

    fix bag

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {   HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 
 import './index.scss';
@@ -7,7 +7,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
+
     <HashRouter >
         <App />
     </HashRouter>
